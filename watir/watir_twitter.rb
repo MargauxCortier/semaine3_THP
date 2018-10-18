bb3d9cafba7a8614411dbc788a24bf54acc6874c
irabespodobna <penkovskaya.irina@gmail.com>
2018-10-18 15:44:18 +0200

    kaka

--- watir/watir_twitter.rb
+++ watir/watir_twitter.rb
@@ -1,21 +1,23 @@
 require 'watir'
+require 'json'
 
 browser = Watir::Browser.new()
 browser.goto 'google.com'
 search_bar = browser.text_field(class: 'gsfi')
 search_bar.set("mairie nanterre + twitter")
 
-#méthode de la barre d'entrée
-search_bar.send_keys(:enter)
-
-#méthode du clic
-#submit_button = browser.button(type:"button")
-#submit_button.click
-
-#browser.driver.manage.timeouts.implicit_wait = 3
+submit_button = browser.button(type:"button")
+submit_button.click
+twitters = Array.new
 search_result_divs = browser.divs(class:"r")
-search_result_divs.each { |div| p div.h3.text }
-sleep(3)
 
+search_result_divs.each { |div| twitters << div.h3.text.split[1][1..-2] }
+p twitters[0]
+
+File.open("/home/irabespodobna/THP/week3/Jeudi/semaine3_THP/watir/array.json","w") do |f|
+f.write(twitters[0].to_json)
+end
+
+sleep(3)
 p "Méfait accompli, fermeture du browser"
 browser.close
